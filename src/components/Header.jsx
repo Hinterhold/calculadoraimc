176f6ba693e67d7132566e280dae66bdbf2a7f83
kaip <kaipacc999@gamil.com>
2025-09-04 21:11:32 -0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../css/global.css'
+
+function Header (){
+    return(
+        <div className='header'>
+            <h1>calculadora IMC</h1>
+
+        </div>
+    )
+}
+export default Header
